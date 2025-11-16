57a2153cf434b0fbecfe3696e640d443257c75b8
shogo0324 <hashimoto.s.f2dc@m.isct.ac.jp>
2025-11-16 22:22:05 +0900

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "blogs/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs do
     resources :likes, only: [:create, :destroy]
@@ -8,7 +7,6 @@ Rails.application.routes.draw do
   resources :mypage
   resources :users, only: [:show]
   root to: 'blogs#index'
-  resources :blogs
 get 'blogs/date/:date', to: 'blogs#table', as: 'blogs_table'
  # ← rootはどれか1つに決める
 end
